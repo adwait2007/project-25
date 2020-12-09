a193726e1550d325d0a4a25f220064429384df0b
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-09 16:58:06 +0530

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,12 +1,12 @@
 class Ground {
-    constructor(x,y,height,width){
+    constructor(x,y,width,height){
        var ground_options = {
            isStatic: true	
       }
       this.x=x;
       this.y=y;
-      this.height=height;
       this.width=width;
+      this.height=height
       this.Body = Bodies.rectangle(this.x,this.y,this.width,this.height,ground_options);
       World.add(world,this.Body);
 
@@ -14,7 +14,7 @@ class Ground {
     display(){
         var pos = this.Body.position;
         rectMode(CENTER);
-        fill(255);
+        fill("brown");
         rect(pos.x,pos.y,this.width,this.height);
     
     }
--- paper.js
+++ paper.js
@@ -1,6 +1,29 @@
-class Paper extends BaseClass{
+class Paper {
     constructor(x,y,r){
-        super(x,y,r)
-        this.image=loadImage("paper.png")
+        var option={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r
+        this.body=Bodies.circle(this.x,this.y,this.r,option);
+        this.image=loadImage("paper.png");
+       
+        World.add(world,this.body)
+    }
+    display(){
+        var paperpos=this.body.position; 
+        push()
+        translate(paperpos.x,paperpos.y);
+        ellipseMode(RADIUS) 
+        
+        fill(255)
+       
+        image(this.image,0,0,this.r*1.2,this.r*1.2);
+       
+        pop()
     }
 }
