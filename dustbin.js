3eecd7f49bdc6aa4af02f4ba6cc5a91ec8a85275
Adwait <74483768+adwait2007@users.noreply.github.com>
2020-12-31 17:21:29 +0530

    Add files via upload

--- dustbin.js
+++ dustbin.js
@@ -1,22 +1,68 @@
-class Dustbin {
-     constructor(x,y,width,height){
-        var dustbin_options = {
-            isStatic: true	
-       }
-       this.x=x;
-       this.y=y;
-       this.width=width;
-       this.height=height;
-       this.Body = Bodies.rectangle(this.x,this.y,this.width,this.height,dustbin_options);
-       
-	    World.add(world,this.Body);
+class Dustbin{
+	constructor(x,y){
 
-     }
-     display(){
-        var pos = this.Body.position;
-        rectMode(CENTER);
-        fill(255,0,0);
-        rect(pos.x,pos.y,this.width,this.height);
-    
-     }
+      var option={
+         isStatic:true
+      }
+
+		this.x=x;
+		this.y=y;
+		this.dustbinWidth=200;
+		this.dustbinHeight=213;
+		this.wallThickness=20;
+		
+		this.image=loadImage("dustbingreen.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness,option )
+		this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight,option )
+		
+		
+
+		this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight,option )
+		
+		World.add(world, this.bottomBody)
+		World.add(world, this.leftWallBody)
+		World.add(world, this.rightWallBody);
+
+	}
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			var posLeft=this.leftWallBody.position;
+			var posRight=this.rightWallBody.position;
+
+			
+
+			push()
+			translate(posLeft.x, posLeft.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posRight.x, posRight.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			rotate(-1*this.angle)
+			//rect(0,0,this.wallThickness, this.dustbinHeight);
+			pop()
+
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			angleMode(RADIANS)
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight)
+			//rect(0,0,this.dustbinWidth, this.wallThickness);
+			pop()
+			
+	}
+
 }
--- sketch.js
+++ sketch.js
@@ -6,10 +6,6 @@ const Body = Matter.Body;
 var paper,dustbin1,dustbin2,dustbin3,ground;
 var dustbin,dustbinIMG;
 
-function preload(){
-    dustbinIMG=loadImage("dustbingreen.png");
-}
-
 function setup() {
 	createCanvas(800, 700);
 
@@ -17,15 +13,9 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	dustbin=createSprite(600,515,5,170);
-	dustbin.addImage(dustbinIMG)
-	dustbin.scale=0.5
-
 	//Create the Bodies Here.
-	paper=new Paper(150,570,50);
-	dustbin1=new Dustbin(600,590,120,10);
-	dustbin2=new Dustbin(655,515,10,160);
-	dustbin3=new Dustbin(545,515,10,160);
+	paper=new Paper(20,570,50);
+	dustbin1=new Dustbin(600,590);
 	ground=new Ground(width/2,600,width,10);
 
 	Engine.run(engine);
@@ -35,12 +25,10 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background("white");
+  background("lightBlue");
 
   paper.display();
   dustbin1.display();
-  dustbin2.display();
-  dustbin3.display();
   ground.display();
   
   drawSprites();
@@ -51,7 +39,7 @@ function keyPressed(){
 	if(keyCode === UP_ARROW){
 
 		// your object is paper so that name you give.. try now
-		Matter.Body.applyForce(paper.body,paper.body.position,{x:370,y:-370})
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:450,y:-450})
 
 
 	}
